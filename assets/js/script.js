18f100c1eb9b3a370edf54bbbed5f306d7217359
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-06-24 17:48:26 -0500

    corrected the class name for taskInfo

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //check if input values are empty
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
    // package data as an object
    var taskDataObj = {
        name: taskNameInput,
@@ -19,7 +25,7 @@ var createTaskEl = function(taskDataObj) {
  //create div to hold task info and add to list item
  var taskInfoEl = document.createElement("div");
  //giv it a class name
- taskInfoEl.className = "task-info";
+ taskInfoEl.className = "task-item";
  //add html content to div
  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
  listItemEl.appendChild(taskInfoEl);
