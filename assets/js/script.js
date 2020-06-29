eea1883956c4aa3bc5925a5242251859046d05bf
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-06-29 14:15:17 -0500

    drag and drop function, with modified style to show where you are dragging

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,6 +51,7 @@ var createTaskEl = function(taskDataObj) {
  listItemEl.className = "task-item";
  // add task id as a custom attribute
  listItemEl.setAttribute("data-task-id", taskIdCounter);
+ listItemEl.setAttribute("draggable", "true");
  //create div to hold task info and add to list item
  var taskInfoEl = document.createElement("div");
  //giv it a class name
@@ -101,6 +102,25 @@ var createTaskActions = function(taskId){
 
     return actionContainerEl;
 }
+var dropTaskHandler = function(event) {
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+    //set status of task based on dropzone id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+    if (statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0;
+    }
+    else if (statusType === "tasks-in-progress"){
+        statusSelectEl.selectedIndex = 1;
+    }
+    else if (statusType === "tasks-completed"){
+        statusSelectEl.selectedIndex = 2;
+    }
+    dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
+  };
 formEl.addEventListener("submit" , taskFormHandler);
 
 var taskButtonHandler = function(event) {
@@ -151,4 +171,27 @@ var taskStatusChangeHandler = function(event){
     tasksCompletedEl.appendChild(taskSelected);
   }
 }
+var dragTaskHandler = function(event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    event.dataTransfer.setData("text/plain", taskId);
+    var getId = event.dataTransfer.getData("text/plain");
+    console.log("getId:", getId, typeof getId);
+}
+  var dropZoneDragHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+      event.preventDefault();
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+    }
+  };
+  var dragLeaveHandler = function(event){
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+      taskListEl.removeAttribute("style");
+    }
+  }
 pageContentEl.addEventListener("change" , taskStatusChangeHandler);
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
+pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave" , dragLeaveHandler);
