cf41942af3bba62c376fb6a643c3ae86bf54cdb8
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-06-29 11:46:15 -0500

    delete button functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -77,3 +79,18 @@ var createTaskActions = function(taskId){
 }
 formEl.addEventListener("submit" , taskFormHandler);
 
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+  
+    if (event.target.matches(".delete-btn")) {
+        //get the elements task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+  };
+
+pageContentEl.addEventListener("click", taskButtonHandler);
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
